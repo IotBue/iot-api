a26bf25a0cdafdafe12cc812db4eff904f8144d8
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2016-04-17 21:34:30 -0300

    Implemented simple socket management to communicate changes

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express    = require('express');
 var app        = express();
+var server     = require('http').Server(app);
 var cors       = require('cors');
 var bodyParser = require('body-parser');
 var mongoose   = require('mongoose');
+var io         = require('socket.io')(server);
 //mongoose.connect('mongodb://localhost/raspberry-api-dev');
 mongoose.connect('mongodb://raspi:raspi@ds011261.mlab.com:11261/iot-raspi-db');
 
@@ -10,13 +12,16 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
-var port = process.env.PORT || 8080;
-
-var routes = require('./app/routes/routes');
+var routes = require('./app/routes/routes')(io);
 app.use('/api', routes);
 
-app.listen(port);
-console.log('UP!');
+server.listen(process.env.PORT || 8080, function() {
+  console.log("UP!");
+});
 
-
+io.on('connection', function(socket) {
+  console.log('User ' + socket.id + ' connected at ' + Date.now());
+  socket.on('disconnect', function() {
+    console.log('User disconnected at ' + Date.now());
+  });
+});
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -4,122 +4,127 @@ var Raspi      = require('../models/raspi');
 var Sensor     = require('../models/sensor');
 var SensorData = require('../models/sensorData');
 
-// POST /api/raspis
-router.post('/raspis', function(request, response) {
-  raspi = new Raspi();
-  raspi.model = request.body.model;
-  raspi.active = request.body.active;
-  raspi.save(function(error, raspi) {
-    if (error) {
-      response.send(error);
-    } else {
-      response.json({ message: 'Raspi creada satisfactoriamente!', raspi: raspi });
-    }
-  });
-});
+module.exports = function(io) {
 
-// GET /api/raspis
-router.get('/raspis', function(request, response) {
-  Raspi.find(function(error, raspis){
-    if (error) {
-      response.send(error);
-    } else {
-      response.send(raspis.map(function(raspi) {
-        return { 
-          id: raspi._id,
-          model: raspi.model, 
-          active: raspi.active,
-          sensors: raspi.sensors.length
-        }
-      }));
-    } 
-  });
-});
-
-// GET /api/:raspi_id/sensors
-router.get('/raspis/:raspi_id/sensors', function(request, response) {
-  Raspi.findById(request.params.raspi_id, function(error, raspi) {
-    if (error) {
-      response.send(error);
-    } else {
-      raspi.populate('sensors', function(error, raspi) {
-        response.send(raspi);
-      });
-    }
-  });
-});
-
-// POST /raspis/:raspi_id/sensors
-router.post('/raspis/:raspi_id/sensors', function(request, response) {
-  var sensor = new Sensor();
-  sensor.pin = request.body.pin;
-  sensor.sensorType = request.body.sensorType;
-  sensor.active = request.body.active;
-  sensor.save(function(error, sensor) {
-    if (error) {
-      response.send(error);
-    } else {
-      Raspi.findById(request.params.raspi_id, function(error, raspi) {
-        raspi.sensors.push(sensor);
-        raspi.save(function(error) {
-          if (error) {
-            response.send(error);
-          } else {
-            response.json({ message: 'Sensor creado satisfactoriamente' });
-          }
-        });
-      });
-    }
-  });
-});
-
-// GET /raspis/:raspi_id/sensors/:sensor_id
-router.get('/raspis/:raspi_id/sensors/:sensor_id', function(request, response) {
-  Raspi.findById(request.params.raspi_id, function(error, raspi) {
-    if (error) {
-      response.send(error)
-    } else {
-      var sensorId = raspi.sensors.find(function(sensorId){
-        return sensorId == request.params.sensor_id;
-      });
-      if (sensorId != undefined) {
-        Sensor.findById(request.params.sensor_id, function(error, sensor){
-          if (error) {
-            response.send(error);
-          } else {
-            sensor.populate('sensorData', function(error, sensor) {
-              response.send(sensor);
-            });
-          }
-        })
+  // POST /api/raspis
+  router.post('/raspis', function(request, response) {
+    raspi = new Raspi();
+    raspi.model = request.body.model;
+    raspi.active = request.body.active;
+    raspi.save(function(error, raspi) {
+      if (error) {
+        response.send(error);
       } else {
-        response.json({ message: 'No hay un sensor con ese ID en la raspi'});
+        response.json({ message: 'Raspi creada satisfactoriamente!', raspi: raspi });
       }
-    }
+    });
   });
-});
 
-// POST /raspis/:raspi_id/sensors/:sensor_id/sensors_data
-router.post('/raspis/:raspi_id/sensors/:sensor_id/sensors_data', function(request, response) {
-  var sensorData = new SensorData();
-  sensorData.value = request.body.value;
-  sensorData.sentAt = request.body.sentAt;
-  sensorData.save(function(error, sensorData) {
-    if (error) {
-      response.send(error);
-    } else {
-      Sensor.findById(request.params.sensor_id, function(error, sensor) {
-        sensor.sensorData.push(sensorData);
-        sensor.save(function(error) {
-          if (error) {
-            response.send(error);
-          } else {
-            response.json({ message: 'Datos creados satisfactoriamente' });
+  // GET /api/raspis
+  router.get('/raspis', function(request, response) {
+    Raspi.find(function(error, raspis){
+      if (error) {
+        response.send(error);
+      } else {
+        response.send(raspis.map(function(raspi) {
+          return { 
+            id: raspi._id,
+            model: raspi.model, 
+            active: raspi.active,
+            sensors: raspi.sensors.length
           }
-        });
-      });
-    }
+        }));
+      } 
+    });
   });
-});
 
-module.exports = router;
+  // GET /api/:raspi_id/sensors
+  router.get('/raspis/:raspi_id/sensors', function(request, response) {
+    Raspi.findById(request.params.raspi_id, function(error, raspi) {
+      if (error) {
+        response.send(error);
+      } else {
+        raspi.populate('sensors', function(error, raspi) {
+          response.send(raspi);
+        });
+      }
+    });
+  });
+
+  // POST /raspis/:raspi_id/sensors
+  router.post('/raspis/:raspi_id/sensors', function(request, response) {
+    var sensor = new Sensor();
+    sensor.pin = request.body.pin;
+    sensor.sensorType = request.body.sensorType;
+    sensor.active = request.body.active;
+    sensor.save(function(error, sensor) {
+      if (error) {
+        response.send(error);
+      } else {
+        Raspi.findById(request.params.raspi_id, function(error, raspi) {
+          raspi.sensors.push(sensor);
+          raspi.save(function(error) {
+            if (error) {
+              response.send(error);
+            } else {
+              response.json({ message: 'Sensor creado satisfactoriamente' });
+            }
+          });
+        });
+      }
+    });
+  });
+
+  // GET /raspis/:raspi_id/sensors/:sensor_id
+  router.get('/raspis/:raspi_id/sensors/:sensor_id', function(request, response) {
+    Raspi.findById(request.params.raspi_id, function(error, raspi) {
+      if (error) {
+        response.send(error)
+      } else {
+        var sensorId = raspi.sensors.find(function(sensorId){
+          return sensorId == request.params.sensor_id;
+        });
+        if (sensorId != undefined) {
+          Sensor.findById(request.params.sensor_id, function(error, sensor){
+            if (error) {
+              response.send(error);
+            } else {
+              sensor.populate('sensorData', function(error, sensor) {
+                response.send(sensor);
+              });
+            }
+          })
+        } else {
+          response.json({ message: 'No hay un sensor con ese ID en la raspi'});
+        }
+      }
+    });
+  });
+
+  // POST /raspis/:raspi_id/sensors/:sensor_id/sensors_data
+  router.post('/raspis/:raspi_id/sensors/:sensor_id/sensors_data', function(request, response) {
+    var sensorData = new SensorData();
+    var sensorId   = request.params.sensor_id;
+    sensorData.value = request.body.value;
+    sensorData.sentAt = request.body.sentAt;
+    sensorData.save(function(error, sensorData) {
+      if (error) {
+        response.send(error);
+      } else {
+        Sensor.findById(request.params.sensor_id, function(error, sensor) {
+          sensor.sensorData.push(sensorData);
+          sensor.save(function(error) {
+            if (error) {
+              response.send(error);
+            } else {
+              response.json({ message: 'Datos creados satisfactoriamente' });
+              io.emit(sensorId, { data: [sensorData.value, sensorData.sentAt] });
+            }
+          });
+        });
+      }
+    });
+  });
+
+  return router;
+};
